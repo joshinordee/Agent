0436ce2b23c93abc6e515a2ec76e10b94c13dede
joshinordee <inordeejoshua04@gmail.com>
2025-07-10 03:42:41 -0500

    connected to gemini API

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents=sys.argv
+)
+if len(sys.argv) < 2:
+    print("Error. Must enter string prompt.")
+    sys.exit(1)
+print(response.text)
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+def main():
+    print("Hello from agent!")
+
+
+if __name__ == "__main__":
+    main()

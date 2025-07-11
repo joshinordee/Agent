060377a21a0273f3b801a67f3727a1ef367b76e0
joshinordee <inordeejoshua04@gmail.com>
2025-07-11 15:47:44 -0500

    added get_file_content and config.py to get file content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,6 @@
 import os
+from functions.config import *
+
 
 def get_files_info(working_directory, directory=None):
     try:
@@ -28,7 +30,32 @@ def get_files_info(working_directory, directory=None):
     
     except Exception as e:
         return f"Error: {str(e)}"
+    
+def get_file_content(working_directory, file_path):
+    try:
+        file_path = os.path.join(working_directory, file_path)
 
+        # absolute paths
+        working_directory_abs_path = os.path.abspath(working_directory)
+        target_path = os.path.abspath(file_path)
+
+        if not target_path.startswith(working_directory_abs_path): # make sure only permitted file_paths ctories are analyzed by the LLM
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        if not os.path.isfile(target_path):
+            return f'Error: File not found or is not a regular file: "{target_path}"'
+    
+        with open(target_path, "r") as f:
+            file_content_string = f.read(CHARACTER_LIMIT)
+
+        if len(file_content_string) == CHARACTER_LIMIT:
+            return f'{file_content_string} [...File "{file_path}" truncated at {CHARACTER_LIMIT} characters]'
+        
+        return file_content_string
+        
+    except OSError as e:
+        return f"Error: {str(e)}"
     
 
+    
 
--- tests.py
+++ tests.py
@@ -1,17 +1,10 @@
 # from subdirectory.filename import function_name
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_file_content
 
-# Tests for get_files_info.py
 
-print("Result for current directory:")
-print(get_files_info("calculator", "."))
+# Tests for get_files_content.py
 
-print('Result for "pkg" directory:')
-print(get_files_info("calculator", "pkg"))
-
-print('Result for "/bin" directory:')
-print(get_files_info("calculator", "/bin"))
-
-print('Result for "../" directory:')
-print(get_files_info("calculator", "../"))
-
+#print(get_file_content("calculator", "main.py"))
+#print(get_file_content("calculator", "pkg/calculator.py"))
+#print(get_file_content("calculator", "/bin/cat"))
+print(get_file_content("calculator", "lorem.txt"))
